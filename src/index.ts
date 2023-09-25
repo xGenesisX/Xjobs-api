f88bdabcf6ab15a43c768726d207651cabd50fbb
sudotx <64108848+sudotx@users.noreply.github.com>
2023-09-25 07:33:10 +0100

    added some rick and morty easter eggs cuz why not

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import morgan from "morgan";
+
+import errorHandler from "./app/middleware/errorHandler";
+
+import chatRoutes from "./app/routes/chat.route";
+import gigRoutes from "./app/routes/gig.route";
+import profileRoutes from "./app/routes/profile.route";
+import proposalRoutes from "./app/routes/proposal.route";
+import contractRoutes from "./app/routes/contract.route";
 
 dotenv.config();
 
@@ -9,13 +18,24 @@ const PORT = process.env.PORT || 4000;
 const app: Express = express();
 
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Sometimes science is more art than science </h1>");
+app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/gig", gigRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/proposal", proposalRoutes);
+app.use("/api/v1/contract", contractRoutes);
+
+app.use(errorHandler);
+
+app.get("/health_check", (req: Request, res: Response) => {
+  res.send(
+    "<h1>Sometimes science is more art than science - Rick Sanchez </h1>"
+  );
 });
 
-app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
+app.listen(PORT, () => console.log(`Getting Schwifty on ${PORT} ⚡`));
 
 export { app };
