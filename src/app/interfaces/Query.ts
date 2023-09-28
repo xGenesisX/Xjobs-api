3fcf1349246c7e1ea9157ba630f720172f0afb53
sudotx <64108848+sudotx@users.noreply.github.com>
2023-09-29 00:48:56 +0100

    code cleanup

--- src/app/interfaces/Query.ts
+++
@@ -1,7 +0,0 @@
-export interface Query {
-  approvedForMenu: boolean;
-  isAwarded: boolean;
-  category?: {
-    value: string[]; // Assuming 'value' is an array of strings, adjust the type accordingly
-  };
-}
