2b7de959a0ac1391b33ff30decb7611a5c8b5a50
sudotx <64108848+sudotx@users.noreply.github.com>
2023-09-29 00:49:49 +0100

    update mailer credentials

--- src/app/utils/mailer.ts
+++ src/app/utils/mailer.ts
@@ -29,11 +29,10 @@ export default class Email {
 
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      // port: process.env.EMAIL_PORT,
-      // auth: {
-      //   user: process.env.EMAIL_USERNAME,
-      //   pass: process.env.EMAIL_PASSWORD,
-      // },
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
     });
   }
 
