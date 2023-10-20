67ecdd4fcaa2e5bd81b35a7ce9a5fd855416d017
sudotx <64108848+sudotx@users.noreply.github.com>
2023-10-20 12:00:05 +0100

    chore: commented out required some config props

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -8,15 +8,15 @@ const envVarsSchema = Joi.object()
       .required(),
     PORT: Joi.number().default(3000),
     MONGO_URL: Joi.string().required().description("Mongo DB url"),
-    SMTP_HOST: Joi.string().description("server that will send the emails"),
-    SMTP_PORT: Joi.number().description("port to connect to the email server"),
-    SMTP_USERNAME: Joi.string().description("username for email server"),
-    SMTP_PASSWORD: Joi.string().description("password for email server"),
-    EMAIL_FROM: Joi.string().description(
-      "the from field in the emails sent by the app"
-    ),
-    CLIENT_URL: Joi.string().required().description("Client url"),
-    ABLY_API_KEY: Joi.string().required().description("ably url"),
+    // SMTP_HOST: Joi.string().description("server that will send the emails"),
+    // SMTP_PORT: Joi.number().description("port to connect to the email server"),
+    // SMTP_USERNAME: Joi.string().description("username for email server"),
+    // SMTP_PASSWORD: Joi.string().description("password for email server"),
+    // EMAIL_FROM: Joi.string().description(
+    //   "the from field in the emails sent by the app"
+    // ),
+    // CLIENT_URL: Joi.string().required().description("Client url"),
+    // ABLY_API_KEY: Joi.string().required().description("ably url"),
   })
   .unknown();
 
