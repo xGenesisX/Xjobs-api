4727f1faa0af9ca3fcf5d214a4ffb312c9a00f2f
sudotx <64108848+sudotx@users.noreply.github.com>
2023-11-26 01:15:19 +0100

    chore: add router and controller for web3 class

--- src/app/controllers/web3.controller.ts
+++ src/app/controllers/web3.controller.ts
@@ -1,1 +1,32 @@
-// call functions on the class
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+import { getToken } from "next-auth/jwt";
+import catchAsync from "../utils/catchAsync";
+import { SPLWeb3 } from "../web3";
+
+export const receiveSOL = catchAsync(async (req: Request, res: Response) => {
+  let token = getToken({ req });
+  if (!token) {
+    return res.status(httpStatus.UNAUTHORIZED);
+  } else {
+    try {
+      const p = new SPLWeb3(0, "mainnet").receiveSOL;
+      res.send(p);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+});
+export const sendSOL = catchAsync(async (req: Request, res: Response) => {
+  let token = getToken({ req });
+  if (!token) {
+    return res.status(httpStatus.UNAUTHORIZED);
+  } else {
+    try {
+      const p = new SPLWeb3(0, "mainnet").sendSOL;
+      res.send(p);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+});
--- src/app/routes/web3.route.ts
+++ src/app/routes/web3.route.ts
@@ -1,24 +1,17 @@
-// is payment validated?
-// get details on a address
 import express, { NextFunction, Request, Response } from "express";
 import { authenticate } from "../middleware/authHandler";
+import { receiveSOL, sendSOL } from "../controllers/web3.controller";
 
 const router = express.Router();
 
 router
-  .route("/send_usdc")
+  .route("/receive_sol")
   .get(authenticate, (req: Request, res: Response, next: NextFunction) => {
-    // getOwnerGigById(req, res, next);
+    receiveSOL(req, res, next);
   });
 
 router
   .route("/send_sol")
   .get(authenticate, (req: Request, res: Response, next: NextFunction) => {
-    // getOwnerGigById(req, res, next);
-  });
-
-router
-  .route("/is_payment_validated")
-  .get(authenticate, (req: Request, res: Response, next: NextFunction) => {
-    // getOwnerGigById(req, res, next);
+    sendSOL(req, res, next);
   });
